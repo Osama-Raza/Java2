e45a2ee1eacac540f57509a1e71ed88950753e52
Osama-Raza <osamaraza988@gmail.com>
2023-10-28 01:45:45 +0500

    girt

---
+++ app.js
@@ -0,0 +1,8 @@
+var myName = ("I m Osama Raza. ")
+var course = ("I m learning javascript. ")
+var teacher = ("Learning from best teacher Sir Rizwan Bhatti.")
+console.log(myName + course + teacher);
+var illegalvariable = ("Spaces between name, javascript keyword,symbols except $ and _ ,Hyphens, Cannot start with number.")
+var legalVariable = (" \n Letters, numbers, dollar sign, underscore")
+console.log(illegalvariable + legalVariable);
+document.write ("<b>Rules for naming JS variables</b>  <br> <br> Variable names can only contain letters, numbers,$, _ For Example $my_1stVariable <br> Variables must begin with a letter, $ , _. For example: $name, _name or name <br> Variable names are case sensitive, <br> Variable names should not be JS keyword")
